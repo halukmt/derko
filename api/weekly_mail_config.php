1251fbd6894f0523645814f4acee2c6f910f699d
halukmt <halukmt@googlemail.com>
2025-12-15 02:01:34 +0100

    fix(weekly-mailer): add additional recipient for healthcheck emails

--- api/weekly_mail_config.php
+++ api/weekly_mail_config.php
@@ -10,7 +10,7 @@ return [
     // Separate token used by healthcheck requests (keep private)
     'health_token' => '3f6b1d9a8c2e4b7f0a9c1d2e3f4b5a6c',
     // Where healthcheck mails should go (admin inbox). Can be a single string or an array of addresses.
-    'health_to' => ['hostmaster@techsulting.de'],
+    'health_to' => ['hostmaster@techsulting.de', 'koraydemirci@gmx.de'],
     // Subject and body for healthcheck mails
     'health_subject' => 'TEST - DERKO - Kontaktformular Check',
     'health_body' => "Prüfung durchgeführt: OK\nZeitstempel: %s\nQuelle: %s\n"
